d06ce2a4f1f56e84bd32b5c694bee575f460f1d9
Edyta Pawlak <edyta.pawlakk@gmail.com>
2023-12-04 11:50:29 +0100

    Remove unused files

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 - `config` - folder with configuration files for docker compose,
 - `events` - pregenerated keri events,
-- `scripts` - folder with scripts, that tests infrastructure.
+- `scripts` - folder with scripts, that tests infrastructure,
+- `keri-cli` - rust project that provides binary for keri events generation.
 
 ## Usage
 
-To start infrastructure run `docker compose up`.
+Run `docker compose up` to start simple network of 3 Witnesses, 1 Watcher and 1 Mesagkesto.
+To build rust binary, run `cargo build --release`. Then, scripts from `scripts` folder can be run.
--- scripts/test_issue.sh
+++ scripts/test_issue.sh
@@ -1,6 +1,6 @@
 alias keri-cli="./target/release/keri-cli"
 MESAGKESTO_ADDRESS="http://172.17.0.1:3236"
-INPUT_DATA_DIR="./events"
+INPUT_DATA_DIR="./payloads"
 
 keri-cli init -a alice
 keri-cli init -a bob 
@@ -13,8 +13,6 @@ ACDC=$(cat "$INPUT_DATA_DIR"/acdc)
 keri-cli issue -a alice -c "$ACDC"
 
 EXN=$(keri-cli exchange -a alice -r bob -c "$ACDC")
-# echo "$EXN"
-# echo "\n"
 
 ALICE_OOBI=$(keri-cli oobi -a alice) 
 
@@ -34,8 +32,7 @@ done
 curl -X POST $MESAGKESTO_ADDRESS -d $(echo "$EXN")
 
 PULL=$(keri-cli pull -a bob)
-# echo "$PULL"
-echo "\n"
+echo "\nPulling bob's messagebox:"
 
 curl -X POST $MESAGKESTO_ADDRESS -d $(echo "$PULL")
 
