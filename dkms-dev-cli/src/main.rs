ebf86f9e0197ba7d9c152b5ec99073d915e8095e
Edyta Pawlak <edyta.pawlakk@gmail.com>
2024-08-09 14:37:31 +0200

    fix: remove unused code

--- dkms-dev-cli/src/main.rs
+++ dkms-dev-cli/src/main.rs
@@ -32,14 +32,6 @@ struct Cli {
     /// Optional name to operate on
     name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
     command: Option<Commands>,
 }
@@ -221,21 +213,6 @@ async fn main() -> Result<(), CliError> {
         println!("Value for name: {name}");
     }
 
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    // match cli.debug {
-    //     0 => println!("Debug mode is off"),
-    //     1 => println!("Debug mode is kind of on"),
-    //     2 => println!("Debug mode is on"),
-    //     _ => println!("Don't be crazy"),
-    // }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
     match cli.command {
         Some(Commands::Init {
             alias,
